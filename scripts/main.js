bafe8529eb01f85c03337e6ebb47d0276ae20022
misi151 <szadlony@gmail.com>
2017-01-14 22:28:57 +0100

    drawer c2

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-console.log("hello")
+var lastPos = {
+    x: 0,
+    y: 0,
+}
+var canvas = document.querySelector("#canvas")
+var brush = canvas.getContext("2d")
+var bool = false
+
+window.addEventListener("mouseOver", function(event){
+    if (event.clientX >= 8 && event.clientX <= 648 && event.clientY >= 8 && event.clientY <= 488) {
+        bool = true
+    }
+})
+window.addEventListener("mousedown", function(event){
+    if(bool)
+})
