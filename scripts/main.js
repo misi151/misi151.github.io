7416a0306dd29d8c6a203b7ca2eef1e63b9cacc2
misi151 <szadlony@gmail.com>
2017-01-14 22:37:37 +0100

    drawer semi-final ver

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,32 @@ var lastPos = {
 }
 var canvas = document.querySelector("#canvas")
 var brush = canvas.getContext("2d")
-var bool = false
+var press = false
 
-window.addEventListener("mouseOver", function(event){
-    if (event.clientX >= 8 && event.clientX <= 648 && event.clientY >= 8 && event.clientY <= 488) {
-        bool = true
+window.addEventListener("mousedown", function(event){
+    if (event.button === 0){
+        press = true
+        lastPos = {
+            x: event.clientX,
+            y: event.clientY
+        }
     }
 })
-window.addEventListener("mousedown", function(event){
-    if(bool)
+window.addEventListener("mouseUp", function(event){
+    press= false
+})
+window.addEventListener("mousemove", function(event){
+    if (event.clientX >= 8 && event.clientX <= 648 && event.clientY >= 8 && event.clientY <= 488) {
+        if(press) {
+            brush.beginPath()
+            brush.moveTo(clientX - 8, clientY - 8)
+            brush.lineTo(event.clientX - 8, event.clientY - 8)
+            brush.stroke()
+
+            lastPos = {
+            x: event.clientX,
+            y: event.clientY
+            }
+        }
+    }
 })
